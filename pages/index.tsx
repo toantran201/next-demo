af9411b56da477f67e15256f7d2b374a17b986bf
toantran201 <toantran201.w@gmail.com>
2022-10-26 02:27:02 +0700

    Draft images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,25 @@
 import type {NextPage} from 'next'
 import Link from "next/link";
+import Image from "next/image";
 //
 import styles from '~/styles/Home.module.scss'
 import variables from '../styles/variables.module.scss'
+//
+import elephant from '~/public/images/elephant.webp'
 
 const Home: NextPage = () => {
   return (
     <div>
       <h1 className={styles.title} style={{color: variables.primaryColor}}>Index</h1>
-
+      <div style={{width: '300px', height: '150px', position: 'relative'}}>
+        <Image
+          src={elephant}
+          alt="Elephant"
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <br/>
       <Link href='/posts'>
         <a style={{color: 'blue', textDecoration: 'underline'}}>Posts</a>
       </Link>
